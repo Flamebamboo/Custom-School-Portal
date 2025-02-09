10f1bcc1a76f3cae727a056cbc63ad649f7c4ddb
Flamebamboo <166450271+Flamebamboo@users.noreply.github.com>
2025-02-09 19:22:35 +1300

    configured for react app ~ took existing images and portal links.

---
+++ src/data/links.js
@@ -0,0 +1,103 @@
+//extracted from Chris HTML - THANKS BRO
+export const portalLinks = {
+  apps: [
+    {
+      name: "Google Calendar",
+      url: "https://calendar.google.com/calendar/r?tab=mc",
+      icon: "img/Calendar.png",
+    },
+    {
+      name: "Gmail",
+      url: "http://mail.burnside.school.nz/",
+      icon: "img/Gmail-logo.png",
+    },
+    {
+      name: "School Kiwi",
+      url: "https://burnside.school.kiwi/",
+      icon: "img/schoolkiwi.png",
+    },
+    {
+      name: "Schoology",
+      url: "https://schoology.burnside.school.nz/home",
+      icon: "img/Schoology-icon.png",
+    },
+  ],
+  quickLinks: [
+    {
+      name: "NZQA - Information",
+      url: "https://sites.google.com/a/burnside.school.nz/ncea-assessment-information-for-students/home",
+    },
+    {
+      name: "Library",
+      url: "http://opac.burnside.school.nz:2000/#!dashboard",
+    },
+    {
+      name: "Student Council",
+      url: "https://160262.wixsite.com/bhsstudentcouncil",
+    },
+    {
+      name: "Senior Division",
+      url: "https://sites.google.com/a/burnside.school.nz/bhs_senior-division/",
+    },
+    {
+      name: "Newsletters & Yearbooks",
+      url: "https://hail.to/burnside-high-school/",
+    },
+    {
+      name: "Club Websites",
+      url: "calculator://1+1",
+    },
+    {
+      name: "Digital Tech Website",
+      url: "https://sites.google.com/burnside.school.nz/bhs-digital-technologies/year-11/11dtp",
+    },
+    {
+      name: "Careers Website",
+      url: "https://burnside.careerwise.school/",
+    },
+  ],
+  forums: [
+    {
+      name: "Counsellors",
+      url: "https://docs.google.com/a/burnside.school.nz/forms/d/e/1FAIpQLSfAD0VPZiA55Weu-N9AzQlD2fYxMSx975cNVjDkokotlyZHvQ/viewform",
+    },
+    {
+      name: "Bullying Incident",
+      url: "https://docs.google.com/a/burnside.school.nz/forms/d/e/1FAIpQLSdcbaV69k85cOqW2vzDzPPKHRMziTa_eLwj4EkN4-4Zn8dhVQ/viewform",
+    },
+    {
+      name: "Public Health Nurse",
+      url: "https://docs.google.com/a/burnside.school.nz/forms/d/e/1FAIpQLSfWvNBM_Hg75wfttEQrMfU48nTKEcTT-q8THa56D4jZmgAJ7g/viewform",
+    },
+  ],
+  studentTools: [
+    {
+      name: "Google Drive",
+      url: "https://drive.google.com/drive/u/0/my-drive",
+    },
+    {
+      name: "ETV",
+      url: "https://login.etv.org.nz/etv/login",
+    },
+    {
+      name: "MyNZQA",
+      url: "https://taku.nzqa.govt.nz/auth/studentlogin",
+    },
+    {
+      name: "Lucidchart",
+      url: "https://lucid.app/users/login",
+    },
+    {
+      name: "Trello",
+      url: "https://trello.com/",
+    },
+    {
+      name: "Autodesk Fusion Drive",
+      url: "https://burnside1375.autodesk360.com/",
+    },
+    {
+      name: "Autodesk Fusion Web",
+      url: "https://fusion.online.autodesk.com/webapp?submit_button=Launch+Autodesk+Fusion",
+    },
+  ],
+};
